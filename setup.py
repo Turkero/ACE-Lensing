81c114251085de2f9e40fce1ebdf10e42fd3ef28
Tunç Turker <ozgen.turker@edu.ufes.br>
2025-07-18 18:38:04 -0300

    Add files via upload
    updated required packages

--- setup.py
+++ setup.py
@@ -4,11 +4,11 @@ setup(
     name='ace_lensing',                    # Name of the package
     version='0.1.0',                    # Package version
     author='tunc',                      # Package author
-    author_email='ozgen.turker@edu.ufes.br',  # Author's email
+    author_email='tuncturker.work@gmail.com',  # Author's email
     description='Accurate Cosmological Emulator for the Lensing PDF',  # Short description
     long_description=open('README.md').read(),      # Read long description from README
     long_description_content_type='text/markdown',  # Format of long description
-    url='https://github.com/Turkero/ace_lensing',     # URL to the package repository
+    url='https://github.com/Turkero/ACE_Lensing',     # URL to the package repository
     packages=find_packages(),           # Packages in the current directory
     include_package_data=True,          # Include data files as per MANIFEST.in
     classifiers=[                       # Classifiers for the package ############## I HAVE NO IDEA WHATS THAT
@@ -19,13 +19,15 @@ setup(
     python_requires='>=3.6',            # Python version requirement
     install_requires=[                  # Dependencies
     'wheel',                            # For installing packages in a binary format. pip can create .whl files, easier dependency resolution.
-    'numpy==2.0.2',                      # For numerical operations
+    'numpy==2.0.2',                     # For numerical operations  
     'pandas>=2.2.3',                    # For data manipulation and analysis
+    'pyarrow>=5.0.0',                   # For reading .parquet files
     'matplotlib>=3.9.2',                # For plotting
     'natsort>=8.3.1',                   # For natural sorting
     'scipy>=1.13.1',                    # For scientific computations
     'scikit-learn>=1.5.2',              # For machine learning tasks
     'xgboost>=2.0.1',                   # For gradient boosting
+    'jupyter==1.1.1',                   # For using notebook
     ],
 )
 
